97f625825b60cc93bb7d0489fa8d9f6942ba5104
Frank <fsacco@andrew.cmu.edu>
2023-08-25 11:03:00 -0400

    Added page view for multiple quotes

--- src/components/QuoteFetcher.js
+++ src/components/QuoteFetcher.js
@@ -1,31 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function QuoteFetcher({ onNewQuote }) {
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
+function QuoteFetcher() {
+  const [quotes, setQuotes] = useState([]);
+  const quotesPerPage = 5; // Change this to the desired number of quotes per page
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetchQuote();
-  }, []);
+    fetchQuotes();
+  }, [currentPage]);
 
-  const fetchQuote = async () => {
+  const fetchQuotes = async () => {
     try {
-      const response = await axios.get('https://api.quotable.io/random');
-      setQuote(response.data.content);
-      setAuthor(response.data.author);
-      onNewQuote(response.data.content, response.data.author);
+      const response = await axios.get(`https://api.quotable.io/quotes?count=${quotesPerPage}&page=${currentPage}`);
+      setQuotes(response.data.results);
     } catch (error) {
-      console.error('Error fetching quote:', error);
+      console.error('Error fetching quotes:', error);
+    }
+  };
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
     }
   };
 
   return (
     <div className="quote-generator">
       <h1>Random Quote Generator</h1>
-      <p>"{quote}"</p>
-      <p>- {author}</p>
-      <button onClick={fetchQuote}>Generate New Quote</button>
+      <div className="quote-list">
+        {quotes.map((quote, index) => (
+          <div key={index} className="quote">
+            <p>"{quote.content}"</p>
+            <p>- {quote.author}</p>
+          </div>
+        ))}
+      </div>
+      <div className="pagination">
+        <button onClick={prevPage} disabled={currentPage === 1}>Previous Page</button>
+        <button onClick={nextPage}>Next Page</button>
+      </div>
     </div>
   );
 }
