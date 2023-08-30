7251983f2c0ca0c010bae2452e0c24ca6697087d
Frank <fsacco@andrew.cmu.edu>
2023-08-30 15:10:47 -0400

    Added Star/Unstar logic with local storage

--- src/components/StarButton.js
+++ src/components/StarButton.js
@@ -1,13 +1,31 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react';
 
 function StarButton({ quoteId }) {
+  const [isStarred, setIsStarred] = useState(false);
+
+  useEffect(() => {
+    const starredQuotes = JSON.parse(localStorage.getItem('starredQuotes')) || [];
+    setIsStarred(starredQuotes.includes(quoteId));
+  }, [quoteId]);
 
   const handleStarClick = () => {
-    console.log(`Quote ${quoteId} marked as favorite`);
+    const starredQuotes = JSON.parse(localStorage.getItem('starredQuotes')) || [];
+
+    if (isStarred) {
+      const updatedStarredQuotes = starredQuotes.filter(id => id !== quoteId);
+      localStorage.setItem('starredQuotes', JSON.stringify(updatedStarredQuotes));
+    } else {
+      const updatedStarredQuotes = [...starredQuotes, quoteId];
+      localStorage.setItem('starredQuotes', JSON.stringify(updatedStarredQuotes));
+    }
+
+    setIsStarred(!isStarred);
   };
 
   return (
-    <button onClick={handleStarClick}>Star</button>
+    <button onClick={handleStarClick}>
+      {isStarred ? 'Unstar' : 'Star'}
+    </button>
   );
 }
 
