0f106adca3e1d180dd7323269e3e4ad99dc88394
Frank <fsacco@andrew.cmu.edu>
2023-09-07 08:21:07 -0400

    added Github button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import QuoteFetcher from '../components/QuoteFetcher';
 import '../App.css';
+import githubImg from '../assets/GitHub-Mark.png'
 
 const Home = () => {
   const [setCurrentQuote] = useState({ content: '', author: '' });
@@ -15,6 +16,11 @@ const Home = () => {
       <h2 className="page-title">Web Quotes</h2>
       <div className="navigation-links">
         <Link to="/favorites" className="fancy-button">View Favorites</Link>
+
+        <a href="https://github.com/frank-monkey/web-quotes" className="github-button">
+        <img src={githubImg} alt="GitHub" width="50" />
+        </a>
+
         <Link to="/search" className="fancy-button">Search Quotes</Link>
       </div>
       <div className="quote-generator">
